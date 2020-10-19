206badeb0d2d3c8ba9ba78bb630daac788818a98
Josh Janikowski <janikows@fvtc.edu>
2020-10-18 21:18:55 -0500

    set up

---
+++ Assets/DragAndShoot.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndShoot : MonoBehaviour
+{
+    public float power = 10f;
+    public Rigidbody2D rb;
+    public Vector2 minPower;
+    public Vector2 maxPower;
+
+    Camera cam;
+    Vector2 force;
+    Vector3 startingPoint;
+    Vector3 endPoint;
+    public LineDirection lD;
+    private void Start()
+    {
+        cam = Camera.main;
+        lD = GetComponent<LineDirection>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("ClickedtoCrag");
+            startingPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startingPoint.z = 15;
+           var goClicked =  GetObjectClickedOn();
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startingPoint.z = 15;
+            lD.Renderline(startingPoint, currentPoint);
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            endPoint.z = 15;
+
+            force = new Vector2(Mathf.Clamp(startingPoint.x - endPoint.x,minPower.x,maxPower.x),Mathf.Clamp(startingPoint.y - endPoint.y, minPower.y, maxPower.y));
+            rb.AddForce(force*power,ForceMode2D.Impulse);
+            lD.EndLine();
+        }
+    }
+
+    public GameObject GetObjectClickedOn()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.Log(hit.transform.gameObject.name);
+        }
+        return hit.transform.gameObject;
+    }
+}
---
+++ Assets/LoginManager.cs
@@ -0,0 +1,61 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField playerName_InputField;
+    public TextMesh feedbacktext;
+    #region Unity Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+
+
+    #region UI Callback Methods
+    public void ConnectToPhotonServer()
+    {
+        if (playerName_InputField != null)
+        {
+            PhotonNetwork.NickName = playerName_InputField.text;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        Debug.Log("Licked");
+
+    }
+
+    #endregion
+
+    #region Photon CallBack Methods
+    public override void OnConnected()
+    {
+        Debug.Log("OnConnected called the server is available can do something now");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master with Player Name: " + PhotonNetwork.NickName);
+        if (feedbacktext != null)
+        {
+            feedbacktext.text = "Connected to master with Player Name: " + PhotonNetwork.NickName.ToString();
+        }
+
+        // base.OnConnectedToMaster();
+        PhotonNetwork.LoadLevel("SampleScene");
+    }
+
+    #endregion
+
+}
